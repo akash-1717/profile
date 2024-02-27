d89c1a1521903c2d2f0a75bf1be9b47ba2f0d913
akash-1717 <akashpersetti17@gmail.com>
2024-02-27 21:31:21 +0530

    added get route for about

--- app.js
+++ app.js
@@ -212,6 +212,10 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
 app.get("/contact", (req, res) => {
     res.render("contact");
 });
